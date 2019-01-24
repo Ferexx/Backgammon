8122e92cfece8892d04d6d3e8b3bb4b3b32af8b4
Real-EviN <evin.kierans100@gmail.com>
2019-01-24 21:26:25 +0000

    Added better dimension management

--- src/Display.java
+++ src/Display.java
@@ -1,7 +1,11 @@
 public class Display {
 
+    public static int WIDTH = 1280, HEIGHT = 720;
+
+    public static final String TITLE = "Backgammon Window - Pre-Alpha v0.5";
+
     Display() {
-        new Window();
+        new Window(WIDTH, HEIGHT, TITLE);
         new Screen();
     }
 }
--- src/Window.java
+++ src/Window.java
@@ -1,24 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
 
+import java.awt.Dimension;
+
 public class Window extends JFrame {
     private ImageIcon image;
     private JLabel label;
 
-    Window() {
+    Window(int width, int height, String title) {
         getContentPane().setBackground(Color.BLACK);
         image = new ImageIcon(getClass().getResource("Graphics/SmallBoard.png"));
         label = new JLabel(image);
         label.setHorizontalAlignment(SwingConstants.RIGHT);     //Position in bottom right
-        label.setVerticalAlignment(SwingConstants.BOTTOM);
+        label.setVerticalAlignment(SwingConstants.TOP);
         add(label);
-        setPreferredSize(new Dimension(1100, 800)); //Ensuring the window always stays the same size
-        setMaximumSize(new Dimension(1100, 800));
-        setMinimumSize(new Dimension(1100, 800));
+        setPreferredSize(new Dimension(width, height)); //Ensuring the window always stays the same size
+        setMaximumSize(new Dimension(width, height));
+        setMinimumSize(new Dimension(width, height));
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-
 
     }
 }
