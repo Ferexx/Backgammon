3806c1d44eec7d7c24dc06773e3104f977391444
Real-EviN <evin.kierans100@gmail.com>
2019-01-24 21:41:55 +0000

    Added title and used JFrame

--- src/Window.java
+++ src/Window.java
@@ -8,19 +8,19 @@ public class Window extends JFrame {
     private JLabel label;
 
     Window(int width, int height, String title) {
+        JFrame frame = new JFrame(title);
         getContentPane().setBackground(Color.BLACK);
         image = new ImageIcon(getClass().getResource("Graphics/SmallBoard.png"));
         label = new JLabel(image);
         label.setHorizontalAlignment(SwingConstants.RIGHT);     //Position in bottom right
-        label.setVerticalAlignment(SwingConstants.TOP);
-        add(label);
-        setPreferredSize(new Dimension(width, height)); //Ensuring the window always stays the same size
-        setMaximumSize(new Dimension(width, height));
-        setMinimumSize(new Dimension(width, height));
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        frame.add(label);
+        frame.setPreferredSize(new Dimension(width, height)); //Ensuring the window always stays the same size
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
     }
 }
-
