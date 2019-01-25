78ea2effa72e9b375f8ab48a8546a7a175d3879c
Evin <evin.kierans100@gmail.com>
2019-01-25 10:24:43 +0000

    Added dice functionality - prints to console

--- src/Dice.java
+++ src/Dice.java
@@ -1,3 +1,11 @@
-public class Dice {
+import java.util.Random;
 
+public class Dice{
+
+    Dice(){
+        Random random =  new Random();
+        int diceRoll1 = random.nextInt(6) + 1;
+        int diceRoll2 = random.nextInt(6) + 1;
+        System.out.println("\nYour dice rolls are " +diceRoll1 + " and " +diceRoll2);
+    }
 }
