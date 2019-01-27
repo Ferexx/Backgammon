775bc1144b8faeaa0ac56c91a7856ed6f3475a21
Real-EviN <evin.kierans100@gmail.com>
2019-01-27 15:03:59 +0000

    Made Board its own class

--- src/Board.java
+++ src/Board.java
@@ -3,17 +3,13 @@ import javax.swing.border.EmptyBorder;
 
 public class Board {
 
-    private JLabel boardLabel;
-    private ImageIcon image;
-
-    public void Board(){
-
+    //MAKING BOARD A REALITY
+    public static void board(Window window){
         Board board = new Board();//sets the board onto the screen from the graphics folder
-        image = new ImageIcon(getClass().getResource("Graphics/SmallBoard.png"));
+        window.image = new ImageIcon(window.getClass().getResource("Graphics/SmallBoard.png"));
         //Giving the board a padding border
-        boardLabel = new JLabel(image);
-        boardLabel.setBorder(new EmptyBorder( 16, 16, 16, 64 ) );
+        Window.boardLabel = new JLabel(window.image);
+        Window.boardLabel.setBorder(new EmptyBorder( 16, 16, 16, 64 ) );
     }
-    //All Board stuff from Window.java will be in here asap
 
 }
--- src/Window.java
+++ src/Window.java
@@ -1,20 +1,16 @@
 //Java swing imports
-import javax.swing.border.EmptyBorder;
 import javax.swing.*;
 
 //Java awt layouts
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.geom.Ellipse2D;
+import java.awt.Dimension;
 
 public class Window extends JFrame {
 
-    //Hello what doink these are not used blyat someone explique pour moi and why were they not up top
     //Used for the this context
     int x, y, width, height;
 
     //Declarations for JFrame, JLabel, and ImageIcon
-    private ImageIcon image;
+    public ImageIcon image;
     public static JLabel boardLabel;
     static JFrame frame = new JFrame();
 
@@ -35,22 +31,12 @@ public class Window extends JFrame {
         //Ensures that the program quits when you hit (X)
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //MAKING BOARD
-        board();
+        //ADDING BOARD
+        Board.board(this);
 
         //ADDING BUTTONS
         Buttons.buttons(this);
     }
-
-    //MAKING BOARD A REALITY
-    public void board(){
-        Board board = new Board();//sets the board onto the screen from the graphics folder
-        image = new ImageIcon(getClass().getResource("Graphics/SmallBoard.png"));
-        //Giving the board a padding border
-        boardLabel = new JLabel(image);
-        boardLabel.setBorder(new EmptyBorder( 16, 16, 16, 64 ) );
-    }
-
 }
 
 
