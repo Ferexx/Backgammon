23fa2edb6e1e554c2635f005968b250fb43c38e2
Ferexx <jack.price99@hotmail.com>
2019-01-28 18:15:10 +0000

    Added a comment or two

--- src/Controller.java
+++ src/Controller.java
@@ -4,7 +4,8 @@ public class Controller {
     public static void main(String args[]) throws Exception {
         try {new Display();
         new Dice();
-        sleep(100);
+        //Adding to avoid race condition where checkers are drawn before board is, resulting in board being drawn over them
+        sleep(25);
         Checker.startingPositions();
         }
         catch (Exception e) {
