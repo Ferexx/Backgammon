6fa76edc2d57dcb4f18525a4093b81be32767822
Ferexx <jack.price99@hotmail.com>
2019-01-28 18:12:32 +0000

    Added a sleep to fix checkers not drawing

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,15 @@
+import static java.lang.Thread.sleep;
+
 public class Controller {
-    public static void main(String args[]) {
-        new Display();
+    public static void main(String args[]) throws Exception {
+        try {new Display();
         new Dice();
+        sleep(100);
         Checker.startingPositions();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -1,8 +1,7 @@
 public class Point {
-    public int xLocation, yLocation;
+
 
     public Point(int x, int y)  {
-        this.xLocation = x;
-        this.yLocation = y;
+
     }
 }
