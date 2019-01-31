f914c469cf9206922c25366f98c1f07df2b45fa0
Evin <evin.kierans100@gmail.com>
2019-01-31 10:05:03 +0000

    Tweaked Dice positioning further

--- src/Dice.java
+++ src/Dice.java
@@ -11,129 +11,127 @@ public class Dice {
 
     }
 
-    public static void drawDice(Window window) {
+    public static void drawP1Dice(Window window) {
 
         Random random = new Random();
         int diceRoll1 = random.nextInt(6) + 1;
         int diceRoll2 = random.nextInt(6) + 1;
-        System.out.println("\nYour dice rolls are " + diceRoll1 + " and " + diceRoll2);
+        System.out.println("\nPlayer 1 - Your dice rolls are " + diceRoll1 + " and " + diceRoll2);
 
-        //PLAYER 1 DICE
-
-        //If playerplaying (currently set in Controller)
-        if(Controller.PlayerPlaying == "Player1") {
-            //PLAYER 1 DICE 1 CREATION
-            if (diceRoll1 == 1) {
-                image1 = new ImageIcon(window.getClass().getResource("Resources/Dice1.jpg"));
-                Window.dieLabel1 = new JLabel(image1);
-                Window.dieLabel1.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll1 == 2) {
-                image1 = new ImageIcon(window.getClass().getResource("Resources/Dice2.jpg"));
-                Window.dieLabel1 = new JLabel(image1);
-                Window.dieLabel1.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll1 == 3) {
-                image1 = new ImageIcon(window.getClass().getResource("Resources/Dice3.jpg"));
-                Window.dieLabel1 = new JLabel(image1);
-                Window.dieLabel1.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll1 == 4) {
-                image1 = new ImageIcon(window.getClass().getResource("Resources/Dice4.jpg"));
-                Window.dieLabel1 = new JLabel(image1);
-                Window.dieLabel1.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll1 == 5) {
-                image1 = new ImageIcon(window.getClass().getResource("Resources/Dice5.jpg"));
-                Window.dieLabel1 = new JLabel(image1);
-                Window.dieLabel1.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll1 == 6) {
-                image1 = new ImageIcon(window.getClass().getResource("Resources/Dice6.jpg"));
-                Window.dieLabel1 = new JLabel(image1);
-                Window.dieLabel1.setBorder(new EmptyBorder(0, 0, 0, 0));
-            }
-
-            //PLAYER 1 DICE 2 CREATION
-            if (diceRoll2 == 1) {
-                image2 = new ImageIcon(window.getClass().getResource("Resources/Dice1.jpg"));
-                Window.dieLabel2 = new JLabel(image2);
-                Window.dieLabel2.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll2 == 2) {
-                image2 = new ImageIcon(window.getClass().getResource("Resources/Dice2.jpg"));
-                Window.dieLabel2 = new JLabel(image2);
-                Window.dieLabel2.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll2 == 3) {
-                image2 = new ImageIcon(window.getClass().getResource("Resources/Dice3.jpg"));
-                Window.dieLabel2 = new JLabel(image2);
-                Window.dieLabel2.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll2 == 4) {
-                image2 = new ImageIcon(window.getClass().getResource("Resources/Dice4.jpg"));
-                Window.dieLabel2 = new JLabel(image2);
-                Window.dieLabel2.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll2 == 5) {
-                image2 = new ImageIcon(window.getClass().getResource("Resources/Dice5.jpg"));
-                Window.dieLabel2 = new JLabel(image2);
-                Window.dieLabel2.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll2 == 6) {
-                image2 = new ImageIcon(window.getClass().getResource("Resources/Dice6.jpg"));
-                Window.dieLabel2 = new JLabel(image2);
-                Window.dieLabel2.setBorder(new EmptyBorder(0, 0, 0, 0));
-            }
+        //PLAYER 1 DICE 1 CREATION
+        if (diceRoll1 == 1) {
+            image1 = new ImageIcon(window.getClass().getResource("Resources/Dice1.jpg"));
+            Window.dieLabel1_1 = new JLabel(image1);
+            Window.dieLabel1_1.setBorder(new EmptyBorder(0, 0, 250, 0));
+        } else if (diceRoll1 == 2) {
+            image1 = new ImageIcon(window.getClass().getResource("Resources/Dice2.jpg"));
+            Window.dieLabel1_1 = new JLabel(image1);
+            Window.dieLabel1_1.setBorder(new EmptyBorder(0, 0, 250, 0));
+        } else if (diceRoll1 == 3) {
+            image1 = new ImageIcon(window.getClass().getResource("Resources/Dice3.jpg"));
+            Window.dieLabel1_1 = new JLabel(image1);
+            Window.dieLabel1_1.setBorder(new EmptyBorder(0, 0, 250, 0));
+        } else if (diceRoll1 == 4) {
+            image1 = new ImageIcon(window.getClass().getResource("Resources/Dice4.jpg"));
+            Window.dieLabel1_1 = new JLabel(image1);
+            Window.dieLabel1_1.setBorder(new EmptyBorder(0, 0, 250, 0));
+        } else if (diceRoll1 == 5) {
+            image1 = new ImageIcon(window.getClass().getResource("Resources/Dice5.jpg"));
+            Window.dieLabel1_1 = new JLabel(image1);
+            Window.dieLabel1_1.setBorder(new EmptyBorder(0, 0, 250, 0));
+        } else if (diceRoll1 == 6) {
+            image1 = new ImageIcon(window.getClass().getResource("Resources/Dice6.jpg"));
+            Window.dieLabel1_1 = new JLabel(image1);
+            Window.dieLabel1_1.setBorder(new EmptyBorder(0, 0, 250, 0));
         }
 
-        //PLAYER 2 DICE
+        //PLAYER 1 DICE 2 CREATION
+        if (diceRoll2 == 1) {
+            image2 = new ImageIcon(window.getClass().getResource("Resources/Dice1.jpg"));
+            Window.dieLabel1_2 = new JLabel(image2);
+            Window.dieLabel1_2.setBorder(new EmptyBorder(0, 0, 250, 0));
+        } else if (diceRoll2 == 2) {
+            image2 = new ImageIcon(window.getClass().getResource("Resources/Dice2.jpg"));
+            Window.dieLabel1_2 = new JLabel(image2);
+            Window.dieLabel1_2.setBorder(new EmptyBorder(0, 0, 250, 0));
+        } else if (diceRoll2 == 3) {
+            image2 = new ImageIcon(window.getClass().getResource("Resources/Dice3.jpg"));
+            Window.dieLabel1_2 = new JLabel(image2);
+            Window.dieLabel1_2.setBorder(new EmptyBorder(0, 0, 250, 0));
+        } else if (diceRoll2 == 4) {
+            image2 = new ImageIcon(window.getClass().getResource("Resources/Dice4.jpg"));
+            Window.dieLabel1_2 = new JLabel(image2);
+            Window.dieLabel1_2.setBorder(new EmptyBorder(0, 0, 250, 0));
+        } else if (diceRoll2 == 5) {
+            image2 = new ImageIcon(window.getClass().getResource("Resources/Dice5.jpg"));
+            Window.dieLabel1_2 = new JLabel(image2);
+            Window.dieLabel1_2.setBorder(new EmptyBorder(0, 0, 250, 0));
+        } else if (diceRoll2 == 6) {
+            image2 = new ImageIcon(window.getClass().getResource("Resources/Dice6.jpg"));
+            Window.dieLabel1_2 = new JLabel(image2);
+            Window.dieLabel1_2.setBorder(new EmptyBorder(0, 0, 250, 0));
+        }
+    }
 
-        //If playerplaying (currently set in Controller)
-        if(Controller.PlayerPlaying == "Player2") {
-            //PLAYER 2 DICE 1 CREATION
-            if (diceRoll1 == 1) {
-                image1 = new ImageIcon(window.getClass().getResource("Resources/Dice1.jpg"));
-                Window.dieLabel1 = new JLabel(image1);
-                Window.dieLabel1.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll1 == 2) {
-                image1 = new ImageIcon(window.getClass().getResource("Resources/Dice2.jpg"));
-                Window.dieLabel1 = new JLabel(image1);
-                Window.dieLabel1.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll1 == 3) {
-                image1 = new ImageIcon(window.getClass().getResource("Resources/Dice3.jpg"));
-                Window.dieLabel1 = new JLabel(image1);
-                Window.dieLabel1.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll1 == 4) {
-                image1 = new ImageIcon(window.getClass().getResource("Resources/Dice4.jpg"));
-                Window.dieLabel1 = new JLabel(image1);
-                Window.dieLabel1.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll1 == 5) {
-                image1 = new ImageIcon(window.getClass().getResource("Resources/Dice5.jpg"));
-                Window.dieLabel1 = new JLabel(image1);
-                Window.dieLabel1.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll1 == 6) {
-                image1 = new ImageIcon(window.getClass().getResource("Resources/Dice6.jpg"));
-                Window.dieLabel1 = new JLabel(image1);
-                Window.dieLabel1.setBorder(new EmptyBorder(0, 0, 0, 0));
-            }
+    public static void drawP2Dice(Window window) {
 
-            //PLAYER 2 DICE 2 CREATION
-            if (diceRoll2 == 1) {
-                image2 = new ImageIcon(window.getClass().getResource("Resources/Dice1.jpg"));
-                Window.dieLabel2 = new JLabel(image2);
-                Window.dieLabel2.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll2 == 2) {
-                image2 = new ImageIcon(window.getClass().getResource("Resources/Dice2.jpg"));
-                Window.dieLabel2 = new JLabel(image2);
-                Window.dieLabel2.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll2 == 3) {
-                image2 = new ImageIcon(window.getClass().getResource("Resources/Dice3.jpg"));
-                Window.dieLabel2 = new JLabel(image2);
-                Window.dieLabel2.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll2 == 4) {
-                image2 = new ImageIcon(window.getClass().getResource("Resources/Dice4.jpg"));
-                Window.dieLabel2 = new JLabel(image2);
-                Window.dieLabel2.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll2 == 5) {
-                image2 = new ImageIcon(window.getClass().getResource("Resources/Dice5.jpg"));
-                Window.dieLabel2 = new JLabel(image2);
-                Window.dieLabel2.setBorder(new EmptyBorder(0, 0, 0, 0));
-            } else if (diceRoll2 == 6) {
-                image2 = new ImageIcon(window.getClass().getResource("Resources/Dice6.jpg"));
-                Window.dieLabel2 = new JLabel(image2);
-                Window.dieLabel2.setBorder(new EmptyBorder(0, 0, 0, 0));
-            }
+        Random random = new Random();
+        int diceRoll1 = random.nextInt(6) + 1;
+        int diceRoll2 = random.nextInt(6) + 1;
+        System.out.println("\nPlayer 2 - Your dice rolls are " + diceRoll1 + " and " + diceRoll2);
+
+        //PLAYER 2 DICE 1 CREATION
+        if (diceRoll1 == 1) {
+            image1 = new ImageIcon(window.getClass().getResource("Resources/Dice1.jpg"));
+            Window.dieLabel2_1 = new JLabel(image1);
+            Window.dieLabel2_1.setBorder(new EmptyBorder(500, 0, 0, 0));
+        } else if (diceRoll1 == 2) {
+            image1 = new ImageIcon(window.getClass().getResource("Resources/Dice2.jpg"));
+            Window.dieLabel2_1 = new JLabel(image1);
+            Window.dieLabel2_1.setBorder(new EmptyBorder(500, 0, 0, 0));
+        } else if (diceRoll1 == 3) {
+            image1 = new ImageIcon(window.getClass().getResource("Resources/Dice3.jpg"));
+            Window.dieLabel2_1 = new JLabel(image1);
+            Window.dieLabel2_1.setBorder(new EmptyBorder(500, 0, 0, 0));
+        } else if (diceRoll1 == 4) {
+            image1 = new ImageIcon(window.getClass().getResource("Resources/Dice4.jpg"));
+            Window.dieLabel2_1 = new JLabel(image1);
+            Window.dieLabel2_1.setBorder(new EmptyBorder(500, 0, 0, 0));
+        } else if (diceRoll1 == 5) {
+            image1 = new ImageIcon(window.getClass().getResource("Resources/Dice5.jpg"));
+            Window.dieLabel2_1 = new JLabel(image1);
+            Window.dieLabel2_1.setBorder(new EmptyBorder(500, 0, 0, 0));
+        } else if (diceRoll1 == 6) {
+            image1 = new ImageIcon(window.getClass().getResource("Resources/Dice6.jpg"));
+            Window.dieLabel2_1 = new JLabel(image1);
+            Window.dieLabel2_1.setBorder(new EmptyBorder(500, 0, 0, 0));
+        }
+
+        //PLAYER 2 DICE 2 CREATION
+        if (diceRoll2 == 1) {
+            image2 = new ImageIcon(window.getClass().getResource("Resources/Dice1.jpg"));
+            Window.dieLabel2_2 = new JLabel(image2);
+            Window.dieLabel2_2.setBorder(new EmptyBorder(500, 0, 0, 0));
+        } else if (diceRoll2 == 2) {
+            image2 = new ImageIcon(window.getClass().getResource("Resources/Dice2.jpg"));
+            Window.dieLabel2_2 = new JLabel(image2);
+            Window.dieLabel2_2.setBorder(new EmptyBorder(500, 0, 0, 0));
+        } else if (diceRoll2 == 3) {
+            image2 = new ImageIcon(window.getClass().getResource("Resources/Dice3.jpg"));
+            Window.dieLabel2_2 = new JLabel(image2);
+            Window.dieLabel2_2.setBorder(new EmptyBorder(500, 0, 0, 0));
+        } else if (diceRoll2 == 4) {
+            image2 = new ImageIcon(window.getClass().getResource("Resources/Dice4.jpg"));
+            Window.dieLabel2_2 = new JLabel(image2);
+            Window.dieLabel2_2.setBorder(new EmptyBorder(500, 0, 0, 0));
+        } else if (diceRoll2 == 5) {
+            image2 = new ImageIcon(window.getClass().getResource("Resources/Dice5.jpg"));
+            Window.dieLabel2_2 = new JLabel(image2);
+            Window.dieLabel2_2.setBorder(new EmptyBorder(500, 0, 0, 0));
+        } else if (diceRoll2 == 6) {
+            image2 = new ImageIcon(window.getClass().getResource("Resources/Dice6.jpg"));
+            Window.dieLabel2_2 = new JLabel(image2);
+            Window.dieLabel2_2.setBorder(new EmptyBorder(500, 0, 0, 0));
         }
     }
 }
--- src/Window.java
+++ src/Window.java
@@ -9,14 +9,17 @@ public class Window extends JFrame {
     //Used for the this context
     int x, y, width, height;
 
-    //Declarations for JFrame and JLabel
+    //WINDOW > JFRAME > JPANEL > JLABEL
+    //Declarations for JFrame, JPanel and JLabel
     protected static JFrame frame = new JFrame();   //frame for containing Label + image in window
 
     protected static JPanel diePanel= new JPanel(); //Adding new diePanel for 2 dieLabels
 
     protected static JLabel boardLabel;             //Label with the board image on it
-    protected static JLabel dieLabel1;              //Label with the Die image on it
-    protected static JLabel dieLabel2;              //Label with the Die image on it
+    protected static JLabel dieLabel1_1;            //Label with the Die image on it
+    protected static JLabel dieLabel1_2;            //Label with the Die image on it
+    protected static JLabel dieLabel2_1;            //Label with the Die image on it
+    protected static JLabel dieLabel2_2;            //Label with the Die image on it
     protected static JTextArea infoLabel;
 
     //Window constructor - takes in width height and the title on top, all from Display
@@ -42,28 +45,35 @@ public class Window extends JFrame {
         Board.board(this);
 
         //ADDING DIE
-        Dice.drawDice(this);
+        Dice.drawP1Dice(this);  //Player 1 Dice Call
+        Dice.drawP2Dice(this);  //Player 2 Dice Call
 
         //ADDING BUTTONS
         Buttons.buttons(this);
 
         //ADDING TEXT
-        JTextArea info = new JTextArea("Welcome to Backgammon!");
+        JTextArea info = new JTextArea("   Welcome       to       Backgammon!    ");
         info.setBounds(20,20,300,500);
         add(info);
 
-        //ADDING DICE TO JPANEL TO JFRAME - STILL TWEAKING
+        //ADDING DICE JLABELS TO JPANEL, AND THEN TO JFRAME - STILL TWEAKING
         diePanel.setOpaque(true);
-        diePanel.add(dieLabel1);
-        diePanel.add(dieLabel2);
-        frame.add(diePanel);
-        //diePanel.setLayout(new BoxLayout(diePanel, BoxLayout.Y_AXIS));
+        diePanel.add(dieLabel1_1);  //Player 1 Dice 1
+        diePanel.add(dieLabel1_2);  //Player 1 Dice 2
+        diePanel.add(dieLabel2_1);  //Player 2 Dice 1
+        diePanel.add(dieLabel2_2);  //Player 2 Dice 2
 
         //Adding Components to the frame and positioning them
+        //diePanel.setLayout(new BoxLayout(diePanel, BoxLayout.Y_AXIS));    //might need for positioning
+        Window.frame.getContentPane().add(BorderLayout.CENTER, Window.diePanel);  //STILL TWEAKING - alternatively might need for tweaking
+
+        //Adding Board JLabel to JFrame
         Window.frame.getContentPane().add(BorderLayout.LINE_END, Window.boardLabel);
-        //Window.frame.getContentPane().add(BorderLayout.CENTER, Window.diePanel);  //STILL TWEAKING
+        //Adding Die JPanel to JFrame
         Window.frame.add(diePanel);
+        //Adding Info JPanel to JFrame
         Window.frame.getContentPane().add(BorderLayout.LINE_START, info);
+        //Setting Jframe to visible
         Window.frame.setVisible(true);
     }
 
