8b64391405d5940df6c31707f364b80f1ae1240c
Evin <evin.kierans100@gmail.com>
2019-01-31 10:38:57 +0000

    Removed sleep(50) again - not necessary anymore

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,3 @@
-import static java.lang.Thread.sleep;
-
 public class Controller {
 
     //PlayerPlaying string will be used for controlling turns and which dice display
@@ -10,7 +8,7 @@ public class Controller {
         try {
             new Display();
             //Adding to avoid race condition where checkers are drawn before board is, resulting in board being drawn over them
-            sleep(50); //not necessary anymore as initPoints takes up enough time
+            //sleep(50); //not necessary anymore as initPoints takes up enough time
             initPoints();
             startingPositions();
             storeCheckers[0].move(pointList[1], "Red");
