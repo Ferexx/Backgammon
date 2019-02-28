9056ac9a42761353f8c81f46900576ae37422f4b
Ferexx <jack.price99@hotmail.com>
2019-02-28 22:07:28 +0000

    Cleanup and fix possibleMoves

--- src/Moves.java
+++ src/Moves.java
@@ -37,46 +37,55 @@ public class Moves {
     }
     public static void possibleMoves(Window window) {
         window.infoLabel.append("\nYour possible moves are: ");
+        int dice1, dice2;
         if(Game.currentPlayer) {
+            dice1 = window.dice1.getDice1();
+            dice2 = window.dice2.getDice2();
             //Bar moves
             if (Game.pointList[24].getCount() != 0) {
-                if (Game.pointList[window.dice1.getDice1()].getCount() == 0 || Game.pointList[window.dice1.getDice1()].getColor() == "Red") {
-                    window.infoLabel.append("BAR-" + Integer.toString(24 + window.dice1.getDice1()) + " ");
+                if (Game.pointList[dice1].getCount() == 0 || Game.pointList[dice1].getColor() == "Red") {
+                    window.infoLabel.append("BAR-" + Integer.toString(24 + dice1) + " ");
                 }
-                if (Game.pointList[window.dice1.getDice2()].getCount() == 0 || Game.pointList[window.dice1.getDice2()].getColor() == "Red") {
-                    window.infoLabel.append("BAR-" + Integer.toString(24 + window.dice1.getDice2()) + " ");
+                if (Game.pointList[dice2].getCount() == 0 || Game.pointList[dice2].getColor() == "Red") {
+                    if(dice1!=dice2) {
+                        window.infoLabel.append("BAR-" + Integer.toString(24 + dice2) + " ");
+                    }
                 }
-                if (Game.pointList[window.dice1.getDice1()].getCount() == 1 && Game.pointList[window.dice1.getDice1()].getColor() == "Black") {
-                    window.infoLabel.append("BAR-" + Integer.toString(24 + window.dice1.getDice1()) + "* ");
+                if (Game.pointList[dice1].getCount() == 1 && Game.pointList[dice1].getColor() == "Black") {
+                    window.infoLabel.append("BAR-" + Integer.toString(24 + dice1) + "* ");
                 }
-                if (Game.pointList[window.dice1.getDice2()].getCount() == 1 && Game.pointList[window.dice1.getDice2()].getColor() == "Black") {
-                    window.infoLabel.append("BAR-" + Integer.toString(24 + window.dice1.getDice2()) + "* ");
+                if (Game.pointList[dice2].getCount() == 1 && Game.pointList[dice2].getColor() == "Black") {
+                    if(dice1!=dice2) {
+                        window.infoLabel.append("BAR-" + Integer.toString(24 + dice2) + "* ");
+                    }
                 }
             }
             for (int i = 0; i < 24; i++) {
                 if (Game.pointList[i].getColor() == "Red" && Game.pointList[i].getCount() != 0) {
-                    if (Game.pointList[i + window.dice1.getDice1()].getCount() == 0 || Game.pointList[i + window.dice1.getDice1()].getColor() == "Red") {
-                        if (i + window.dice1.getDice1() < 24) {
-                            window.infoLabel.append(Integer.toString(i) + "-" + Integer.toString(i + window.dice1.getDice1()) + " ");
+                    if (Game.pointList[i + dice1].getCount() == 0 || Game.pointList[i + dice1].getColor() == "Red") {
+                        if (i + dice1 < 24) {
+                            window.infoLabel.append(Integer.toString(i) + "-" + Integer.toString(i + dice1) + " ");
                         }
                     }
-                    if (Game.pointList[i + window.dice1.getDice2()].getCount() == 0 || Game.pointList[i + window.dice1.getDice2()].getColor() == "Red") {
-                        if (i + window.dice1.getDice2() < 24) {
-                            window.infoLabel.append(Integer.toString(i) + "-" + Integer.toString(i + window.dice1.getDice2()) + " ");
+                    if (Game.pointList[i + dice2].getCount() == 0 || Game.pointList[i + dice2].getColor() == "Red") {
+                        if (i + dice2 < 24 && dice1!=dice2) {
+                            window.infoLabel.append(Integer.toString(i) + "-" + Integer.toString(i + dice2) + " ");
                         }
                     }
-                    if (Game.pointList[i + window.dice1.getDice1()].getColor() == "Black" && Game.pointList[i + window.dice1.getDice1()].getCount() == 1) {
-                        window.infoLabel.append(Integer.toString(i) + "-" + Integer.toString(i + window.dice1.getDice1()) + "* ");
+                    if (Game.pointList[i + dice1].getColor() == "Black" && Game.pointList[i + dice1].getCount() == 1) {
+                        window.infoLabel.append(Integer.toString(i) + "-" + Integer.toString(i + dice1) + "* ");
                     }
-                    if (Game.pointList[i + window.dice1.getDice2()].getColor() == "Black" && Game.pointList[i + window.dice1.getDice2()].getCount() == 1) {
-                        window.infoLabel.append(Integer.toString(i) + "=" + Integer.toString(i + window.dice1.getDice2()) + "* ");
+                    if (Game.pointList[i + dice2].getColor() == "Black" && Game.pointList[i + dice2].getCount() == 1) {
+                        if(dice1!=dice2) {
+                            window.infoLabel.append(Integer.toString(i) + "=" + Integer.toString(i + dice2) + "* ");
+                        }
                     }
                     //Bear-off moves
                     if (checkBearOffPossible()) {
-                        if ((i + window.dice1.getDice1()) > 23) {
+                        if ((i + dice1) > 23) {
                             window.infoLabel.append(i + "-OFF ");
                         }
-                        if ((i + window.dice1.getDice2()) > 23) {
+                        if ((i + dice2) > 23 && dice1!=dice2) {
                             window.infoLabel.append(i + "-OFF ");
                         }
                     }
@@ -84,45 +93,51 @@ public class Moves {
             }
         }
         else {
+            dice1 = window.dice2.getDice1();
+            dice2 = window.dice2.getDice2();
             //Bar moves
             if (Game.pointList[25].getCount() != 0) {
-                if (Game.pointList[24 - window.dice2.getDice1()].getCount() == 0 || Game.pointList[24 - window.dice2.getDice1()].getColor() == "Black") {
-                    window.infoLabel.append("BAR-" + Integer.toString(window.dice2.getDice1()) + " ");
+                if (Game.pointList[24 - dice1].getCount() == 0 || Game.pointList[24 - dice1].getColor() == "Black") {
+                    window.infoLabel.append("BAR-" + Integer.toString(dice1) + " ");
                 }
-                if (Game.pointList[24 - window.dice2.getDice2()].getCount() == 0 || Game.pointList[24 - window.dice2.getDice2()].getColor() == "Black") {
-                    window.infoLabel.append("BAR-" + Integer.toString(window.dice2.getDice2()) + " ");
+                if (Game.pointList[24 - dice2].getCount() == 0 || Game.pointList[24 - dice2].getColor() == "Black") {
+                    if(dice1!=dice2) {
+                        window.infoLabel.append("BAR-" + Integer.toString(dice2) + " ");
+                    }
                 }
-                if (Game.pointList[24 - window.dice2.getDice1()].getCount() == 1 && Game.pointList[24 - window.dice2.getDice1()].getColor() == "Red") {
-                    window.infoLabel.append("BAR-" + Integer.toString(window.dice2.getDice1()) + "* ");
+                if (Game.pointList[24 - dice1].getCount() == 1 && Game.pointList[24 - dice1].getColor() == "Red") {
+                    window.infoLabel.append("BAR-" + Integer.toString(dice1) + "* ");
                 }
-                if (Game.pointList[24 - window.dice2.getDice2()].getCount() == 1 && Game.pointList[24 - window.dice2.getDice2()].getColor() == "Red") {
-                    window.infoLabel.append("BAR-" + Integer.toString(window.dice2.getDice2()) + "* ");
+                if (Game.pointList[24 - dice2].getCount() == 1 && Game.pointList[24 - dice2].getColor() == "Red") {
+                    if(dice1!=dice2) {
+                        window.infoLabel.append("BAR-" + Integer.toString(dice2) + "* ");
+                    }
                 }
             }
             for (int i = 23; i >= 0; i--) {
                 if (Game.pointList[i].getColor() == "Black" && Game.pointList[i].getCount() != 0) {
-                    if (Game.pointList[i - window.dice2.getDice1()].getCount() == 0 || Game.pointList[i - window.dice2.getDice1()].getColor() == "Black") {
-                        if (i - window.dice2.getDice1() >= 0) {
-                            window.infoLabel.append(Integer.toString(23-i) + "-" + Integer.toString(23-i + window.dice2.getDice1()) + " ");
+                    if (Game.pointList[i - dice1].getCount() == 0 || Game.pointList[i - dice1].getColor() == "Black") {
+                        if (i - dice1 >= 0) {
+                            window.infoLabel.append(Integer.toString(23-i) + "-" + Integer.toString(23-i + dice1) + " ");
                         }
                     }
-                    if (Game.pointList[i - window.dice2.getDice2()].getCount() == 0 || Game.pointList[i - window.dice2.getDice2()].getColor() == "Black") {
-                        if (i - window.dice2.getDice2() >= 0) {
-                            window.infoLabel.append(Integer.toString(23-i) + "-" + Integer.toString(23 - i + window.dice2.getDice2()) + " ");
+                    if (Game.pointList[i - dice2].getCount() == 0 || Game.pointList[i - dice2].getColor() == "Black") {
+                        if (i - dice2 >= 0 && dice1!=dice2) {
+                            window.infoLabel.append(Integer.toString(23-i) + "-" + Integer.toString(23 - i + dice2) + " ");
                         }
                     }
-                    if (Game.pointList[i - window.dice2.getDice1()].getColor() == "Red" && Game.pointList[i - window.dice2.getDice1()].getCount() == 1) {
-                        window.infoLabel.append(Integer.toString(23-i) + "-" + Integer.toString(23 - i + window.dice2.getDice1()) + "* ");
+                    if (Game.pointList[i - dice1].getColor() == "Red" && Game.pointList[i - dice1].getCount() == 1) {
+                        window.infoLabel.append(Integer.toString(23-i) + "-" + Integer.toString(23 - i + dice1) + "* ");
                     }
-                    if (Game.pointList[i - window.dice2.getDice2()].getColor() == "Red" && Game.pointList[i - window.dice2.getDice2()].getCount() == 1) {
-                        window.infoLabel.append(Integer.toString(23-i) + "-" + Integer.toString( 23 - i + window.dice2.getDice2()) + "* ");
+                    if (Game.pointList[i - dice2].getColor() == "Red" && Game.pointList[i - dice2].getCount() == 1 && dice1!=dice2) {
+                        window.infoLabel.append(Integer.toString(23-i) + "-" + Integer.toString( 23 - i + dice2) + "* ");
                     }
                     //Bear-off moves
                     if (checkBearOffPossible()) {
-                        if ((i - window.dice2.getDice1()) < 0) {
+                        if ((i - dice1) < 0) {
                             window.infoLabel.append(23-i + "-OFF ");
                         }
-                        if ((i + window.dice2.getDice2()) < 0) {
+                        if ((i + dice2) < 0 && dice1 != dice2) {
                             window.infoLabel.append(23-i + "-OFF ");
                         }
                     }
